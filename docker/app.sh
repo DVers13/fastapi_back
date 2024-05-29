6469a66e583ba1b4ce004b9419eef0bcd657515d
DVers13 <91010704+DVers13@users.noreply.github.com>
2024-05-29 18:08:22 +0600

    fix9 gg

--- docker/app.sh
+++ docker/app.sh
@@ -6,4 +6,4 @@ cd src
 
 # gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
 
-uvicorn main:app --host 0.0.0.0 --port 80
+uvicorn main:app --host 149.154.70.230 --port 80
