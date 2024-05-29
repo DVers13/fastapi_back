99c5fa71af7ffc29d35404c18edb5dce4443e2f3
DVers13 <91010704+DVers13@users.noreply.github.com>
2024-05-29 19:34:15 +0600

    bearer_transport

--- src/auth/base_config.py
+++ src/auth/base_config.py
@@ -12,15 +12,16 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_async_session
 
-cookie_transport = CookieTransport(cookie_name="cookie", cookie_max_age=3600)
+from fastapi_users.authentication import BearerTransport
 
+bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
 
 def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(secret=SECRET_AUTH, lifetime_seconds=3600)
 
 auth_backend = AuthenticationBackend(
     name="jwt",
-    transport=cookie_transport,
+    transport=bearer_transport,
     get_strategy=get_jwt_strategy,
 )
 
