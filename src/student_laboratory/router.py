f0f43f2e04afc81a0b43ee3931336c558a49c815
DVers13 <91010704+DVers13@users.noreply.github.com>
2024-05-30 22:01:17 +0600

    add new routers student labs

--- src/student_laboratory/router.py
+++ src/student_laboratory/router.py
@@ -1,20 +1,63 @@
-from fastapi import APIRouter, Depends
-from sqlalchemy import select, insert
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import delete, select, insert, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_async_session
 from student_laboratory.models import student_laboratory
-# from student_laboratory.schemas import SLaboratoryCreate
+from student_laboratory.schemas import StudentLaboratoryCreate, StudentLaboratoryUpdate
 
 router = APIRouter(
     prefix="/student_laboratory",
     tags=["student_laboratory"]
 )
 
-@router.get("/")
+@router.get("/get_student_laboratory_by_id")
 async def get_student_laboratory_by_id(student_laboratory_id: int, session: AsyncSession = Depends(get_async_session)):
     query = select(student_laboratory).where(student_laboratory.c.id == student_laboratory_id)
     result = await session.execute(query)
     return result.all()
 
+@router.post("/add_student_laboratory")
+async def add_student_laboratory(new_student_laboratory: Annotated[StudentLaboratoryCreate, Depends()], session: AsyncSession = Depends(get_async_session)):
+    stmt = insert(student_laboratory).values(**new_student_laboratory.dict())
+    await session.execute(stmt)
+    await session.commit()
+    return {"status": "success"}
 
+@router.patch("/update_student_laboratory/") #, dependencies=[Depends(check_permissions(["write"]))]
+async def update_student_laboratory(student_laboratory_id: int, update_data: StudentLaboratoryUpdate, session: AsyncSession = Depends(get_async_session)):
+    update_dict = {k: v for k, v in update_data.dict().items() if v is not None}
+    
+    if not update_dict:
+        raise HTTPException(status_code=400, detail="No data provided for update")
+    
+    if not update_dict:
+        raise HTTPException(status_code=400, detail="No data provided for update")
+
+    stmt = (
+        update(student_laboratory)
+        .where(student_laboratory.c.id == student_laboratory_id)
+        .values(**update_dict)
+    )
+
+    result = await session.execute(stmt)
+    await session.commit()
+
+    if result.rowcount == 0:
+        raise HTTPException(status_code=404, detail="Discipline not found")
+
+    return {"status": "success"}
+
+@router.delete("/delete_student_laboratory_by_id")
+async def delete_discipline_by_id(student_laboratory_id: int, session: AsyncSession = Depends(get_async_session)):
+    stmt = delete(student_laboratory).where(student_laboratory.c.id == student_laboratory_id)
+    await session.execute(stmt)
+    await session.commit()
+    return {"status": "success"}
+
+@router.get("/get_all_student_laboratory")
+async def get_all_subject(session: AsyncSession = Depends(get_async_session)):
+    query = select(student_laboratory)
+    result = await session.execute(query)
+    return result.mappings().all()
--- src/student_laboratory/schemas.py
+++ src/student_laboratory/schemas.py
@@ -2,6 +2,10 @@ from datetime import datetime
 from pydantic import BaseModel
 
 class StudentLaboratoryCreate(BaseModel):
+    id_lab: int
     name: str
     url: str
     
+class StudentLaboratoryUpdate(BaseModel):
+    name: str
+    url: str
