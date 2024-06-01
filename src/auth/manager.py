0d2e6b6a56b2889e597c3fb16c1156f194968562
DVers13 <91010704+DVers13@users.noreply.github.com>
2024-06-01 11:38:25 +0600

    fix2

--- src/auth/manager.py
+++ src/auth/manager.py
@@ -34,7 +34,7 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
         )
         password = user_dict.pop("password")
         user_dict["hashed_password"] = self.password_helper.hash(password)
-        # user_dict["role_id"] = 2 #student
+        user_dict["role_id"] = 2 #student
         created_user = await self.user_db.create(user_dict)
 
         await self.on_after_register(created_user, request)
