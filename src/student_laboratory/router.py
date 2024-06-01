ea2b2fda53cee8cab39f694cc563e3ba46a82125
DVers13 <91010704+DVers13@users.noreply.github.com>
2024-06-01 15:24:16 +0600

    add filter

--- src/student_laboratory/router.py
+++ src/student_laboratory/router.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy import delete, select, insert, update
+from fastapi import APIRouter, Depends, HTTPException, Query
+from sqlalchemy import and_, delete, select, insert, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_async_session
@@ -22,34 +22,39 @@ async def get_student_laboratory_by_id(student_laboratory_id: int, session: Asyn
     return result.all()
 
 @router.get("/get_all_student_laboratory_for_teacher", dependencies=[Depends(check_permissions(["get_student_laboratory_for_teacher"]))])
-async def get_student_laboratory_for_teacher(user: User = Depends(current_user), session: AsyncSession = Depends(get_async_session)):
+async def get_student_laboratory_for_teacher(user: User = Depends(current_user),
+                                        session: AsyncSession = Depends(get_async_session),
+                                        is_personally: bool = Query(False)):
     teacher_id = user.id
+    labs_without_teacher = []
 
-    query_with_teacher = (select(student_laboratory)
-                          .where(student_laboratory.c.id_teacher == teacher_id,
-                                student_laboratory.c.status == False,
-                                student_laboratory.c.valid == True))
+    filters = [student_laboratory.c.status == False, student_laboratory.c.valid == True]
+    # if status is not None:
+    #     filters.append(student_laboratory.c.status == status) status: bool = Query(None), в get_student_laboratory_for_teacher в конец
+    # if valid is not None:
+    #     filters.append(student_laboratory.c.valid == valid)
+    # if min_score is not None:
+    #     filters.append(student_laboratory.c.score >= min_score)
+    # if max_score is not None:
+    #     filters.append(student_laboratory.c.score <= max_score)
+
+    query_with_teacher = select(student_laboratory).where(student_laboratory.c.id_teacher == teacher_id)
+    if not is_personally:
+        query_without_teacher = select(student_laboratory).join(laboratory, laboratory.c.id == student_laboratory.c.id_lab).where(student_laboratory.c.id_teacher.is_(None))
+        query_without_teacher = query_without_teacher.where(and_(*filters))
+        result_disciplines = await session.execute(select(discipline_teacher.c.discipline_id).where(discipline_teacher.c.teacher_id == teacher_id))
+        discipline_ids = [row.discipline_id for row in result_disciplines]
+        query_without_teacher = query_without_teacher.where(laboratory.c.discipline_id.in_(discipline_ids))
+        result_without_teacher = await session.execute(query_without_teacher)
+        labs_without_teacher = result_without_teacher.mappings().all()
+
+    query_with_teacher = query_with_teacher.where(and_(*filters))
+
     result_with_teacher = await session.execute(query_with_teacher)
     labs_with_teacher = result_with_teacher.mappings().all()
 
-    disciplines_query = select(discipline_teacher.c.discipline_id).where(discipline_teacher.c.teacher_id == teacher_id)
-    result_disciplines = await session.execute(disciplines_query)
-    discipline_ids = [row.discipline_id for row in result_disciplines]
-
-    query_without_teacher = (
-        select(student_laboratory)
-        .join(laboratory, laboratory.c.id == student_laboratory.c.id_lab)
-        .where(
-            student_laboratory.c.id_teacher.is_(None),
-            laboratory.c.discipline_id.in_(discipline_ids),
-            student_laboratory.c.status == False,
-            student_laboratory.c.valid == True)
-        )
-    result_without_teacher = await session.execute(query_without_teacher)
-    labs_without_teacher = result_without_teacher.mappings().all()
-
     all_labs = labs_with_teacher + labs_without_teacher
-    sorted_labs = sorted(all_labs, key=lambda lab: lab.id)
+    sorted_labs = sorted(all_labs, key=lambda lab: lab['id'])
     return sorted_labs
 
 @router.post("/add_student_laboratory")
