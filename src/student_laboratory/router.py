79e61574d699f3fade92852ce4ee604156c346ef
DVers13 <91010704+DVers13@users.noreply.github.com>
2024-06-01 16:42:54 +0600

    add more filter

--- src/student_laboratory/router.py
+++ src/student_laboratory/router.py
@@ -5,7 +5,7 @@ from sqlalchemy import and_, delete, select, insert, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_async_session
-from auth.models import User
+from auth.models import User, user
 from laboratory.models import discipline_teacher, laboratory
 from student_laboratory.models import student_laboratory
 from student_laboratory.schemas import StudentLaboratoryCreate, StudentLaboratoryUpdate
@@ -22,30 +22,35 @@ async def get_student_laboratory_by_id(student_laboratory_id: int, session: Asyn
     return result.all()
 
 @router.get("/get_all_student_laboratory_for_teacher", dependencies=[Depends(check_permissions(["get_student_laboratory_for_teacher"]))])
-async def get_student_laboratory_for_teacher(user: User = Depends(current_user),
+async def get_student_laboratory_for_teacher(user_tg: User = Depends(current_user),
                                         session: AsyncSession = Depends(get_async_session),
-                                        is_personally: bool = Query(False)):
-    teacher_id = user.id
+                                        is_personally: bool = Query(False),
+                                        discipline_id: int = Query(None),
+                                        group_id: int = Query(None)):
+    teacher_id = user_tg.id
     labs_without_teacher = []
 
     filters = [student_laboratory.c.status == False, student_laboratory.c.valid == True]
-    # if status is not None:
-    #     filters.append(student_laboratory.c.status == status) status: bool = Query(None), в get_student_laboratory_for_teacher в конец
-    # if valid is not None:
-    #     filters.append(student_laboratory.c.valid == valid)
-    # if min_score is not None:
-    #     filters.append(student_laboratory.c.score >= min_score)
-    # if max_score is not None:
-    #     filters.append(student_laboratory.c.score <= max_score)
+    if discipline_id is not None:
+        filters.append(student_laboratory.c.id_discipline == discipline_id)
+    if group_id is not None:
+        filters.append(user.c.group_id == group_id)
 
-    query_with_teacher = select(student_laboratory).where(student_laboratory.c.id_teacher == teacher_id)
+    query_with_teacher = select(student_laboratory).join(laboratory, laboratory.c.id == student_laboratory.c.id_lab).join(user, user.c.id == student_laboratory.c.id_student).where(student_laboratory.c.id_teacher == teacher_id)
     if not is_personally:
-        query_without_teacher = select(student_laboratory).join(laboratory, laboratory.c.id == student_laboratory.c.id_lab).where(student_laboratory.c.id_teacher.is_(None))
+        
+        query_without_teacher = select(student_laboratory).join(laboratory, laboratory.c.id == student_laboratory.c.id_lab).join(user, user.c.id == student_laboratory.c.id_student).where(student_laboratory.c.id_teacher.is_(None))
+        
         query_without_teacher = query_without_teacher.where(and_(*filters))
+
         result_disciplines = await session.execute(select(discipline_teacher.c.discipline_id).where(discipline_teacher.c.teacher_id == teacher_id))
+        
         discipline_ids = [row.discipline_id for row in result_disciplines]
+        
         query_without_teacher = query_without_teacher.where(laboratory.c.discipline_id.in_(discipline_ids))
+        
         result_without_teacher = await session.execute(query_without_teacher)
+        
         labs_without_teacher = result_without_teacher.mappings().all()
 
     query_with_teacher = query_with_teacher.where(and_(*filters))
