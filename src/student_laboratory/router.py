0a7b52f0bc0706bf95a0508ee52004930e6d3ac8
DVers13 <91010704+DVers13@users.noreply.github.com>
2024-06-02 22:15:05 +0600

    fix get_all_student_laboratory_for_teacher

--- src/student_laboratory/router.py
+++ src/student_laboratory/router.py
@@ -6,9 +6,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_async_session
 from auth.models import User, user
-from laboratory.models import discipline_teacher, laboratory
+from laboratory.models import discipline_teacher, laboratory, subject, discipline
 from student_laboratory.models import student_laboratory
-from student_laboratory.schemas import StudentLaboratoryCreate, StudentLaboratoryUpdate
+from student_laboratory.schemas import DisciplineInfo, StudentInfo, StudentLaboratoryCreate, StudentLaboratoryForTeacher, StudentLaboratoryUpdate
 from auth.base_config import check_permissions, current_user
 router = APIRouter(
     prefix="/student_laboratory",
@@ -26,7 +26,7 @@ async def get_student_laboratory_for_teacher(user_tg: User = Depends(current_use
                                         session: AsyncSession = Depends(get_async_session),
                                         is_personally: bool = Query(False),
                                         discipline_id: int = Query(None),
-                                        group_id: int = Query(None)):
+                                        group_id: int = Query(None)) -> list[StudentLaboratoryForTeacher]:
     teacher_id = user_tg.id
     labs_without_teacher = []
 
@@ -37,20 +37,14 @@ async def get_student_laboratory_for_teacher(user_tg: User = Depends(current_use
         filters.append(user.c.group_id == group_id)
 
     query_with_teacher = select(student_laboratory).join(laboratory, laboratory.c.id == student_laboratory.c.id_lab).join(user, user.c.id == student_laboratory.c.id_student).where(student_laboratory.c.id_teacher == teacher_id)
+    
     if not is_personally:
-        
         query_without_teacher = select(student_laboratory).join(laboratory, laboratory.c.id == student_laboratory.c.id_lab).join(user, user.c.id == student_laboratory.c.id_student).where(student_laboratory.c.id_teacher.is_(None))
-        
         query_without_teacher = query_without_teacher.where(and_(*filters))
-
         result_disciplines = await session.execute(select(discipline_teacher.c.discipline_id).where(discipline_teacher.c.teacher_id == teacher_id))
-        
         discipline_ids = [row.discipline_id for row in result_disciplines]
-        
         query_without_teacher = query_without_teacher.where(laboratory.c.discipline_id.in_(discipline_ids))
-        
         result_without_teacher = await session.execute(query_without_teacher)
-        
         labs_without_teacher = result_without_teacher.mappings().all()
 
     query_with_teacher = query_with_teacher.where(and_(*filters))
@@ -60,10 +54,52 @@ async def get_student_laboratory_for_teacher(user_tg: User = Depends(current_use
 
     all_labs = labs_with_teacher + labs_without_teacher
     sorted_labs = sorted(all_labs, key=lambda lab: lab['id'])
-    return sorted_labs
+    finish_laboratory = []
+    for lab in sorted_labs:
+
+        query = select(user.c.username).where(user.c.id == lab.id_student)
+        result_query = await session.execute(query)
+        username = result_query.mappings().first()
+        student = StudentInfo(id = lab.id_student, name = username.username)
+
+        query = (select(subject.c.name)
+                 .join(discipline, discipline.c.subject_id == subject.c.id)
+                 .where(discipline.c.id == lab.id_discipline))
+        result_query = await session.execute(query)
+        subjectname = result_query.mappings().first()
+        discipline_info = DisciplineInfo(id = lab.id_discipline, name = subjectname.name)
+
+        query = (select(laboratory.c.url, laboratory.c.deadline)
+                 .where(laboratory.c.id == lab.id_lab))
+        result_query = await session.execute(query)
+        laboratory_info = result_query.mappings().first()
+        deadline = laboratory_info.deadline
+        url_task = laboratory_info.url
+        laboratory_teach = StudentLaboratoryForTeacher(student_laboratory_id = lab.id,
+                                                        student = student,
+                                                        discipline = discipline_info,
+                                                        deadline = deadline,
+                                                        loading_time = lab.loading_time,
+                                                        url_task = url_task,
+                                                        url_student_task = lab.url,
+                                                        status = lab.status,
+                                                        valid = lab.valid,
+                                                        count_try = lab.count_try)
+        finish_laboratory.append(laboratory_teach)
+    return finish_laboratory
 
 @router.post("/add_student_laboratory")
 async def add_student_laboratory(new_student_laboratory: Annotated[StudentLaboratoryCreate, Depends()], user: User = Depends(current_user), session: AsyncSession = Depends(get_async_session)):
+    
+    query = (select(student_laboratory.c.id_lab).where(student_laboratory.c.id_student == user.id))
+    result = await session.execute(query)
+    result_dict = result.mappings().all()
+    print(result_dict)
+    id_labs_valid = [lab.id_lab for lab in result_dict]
+
+    if new_student_laboratory.id_lab in id_labs_valid:
+        raise HTTPException(status_code=404, detail="Student laboratory already created")
+    
     new_student_laboratory_dict = new_student_laboratory.model_dump()
     new_student_laboratory_dict["id_student"] = user.id
     new_student_laboratory_dict["status"] = False
--- src/student_laboratory/schemas.py
+++ src/student_laboratory/schemas.py
@@ -11,3 +11,22 @@ class StudentLaboratoryCreate(BaseModel):
 class StudentLaboratoryUpdate(BaseModel):
     student_laboratory_id: int
     url: str
+
+class StudentInfo(BaseModel):
+    id: int
+    name: str
+
+class DisciplineInfo(BaseModel):
+    id: int
+    name: str
+class StudentLaboratoryForTeacher(BaseModel):
+    student_laboratory_id: int
+    student: StudentInfo
+    discipline: DisciplineInfo
+    deadline: datetime
+    loading_time: datetime
+    url_task: str
+    url_student_task: str
+    status: bool
+    valid: bool
+    count_try: int
