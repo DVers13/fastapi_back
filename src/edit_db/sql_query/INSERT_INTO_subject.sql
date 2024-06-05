90315bc1a0ace02dca0a65866122fc53059a4424
DVers13 <91010704+DVers13@users.noreply.github.com>
2024-06-05 19:37:36 +0600

    new query

--- src/edit_db/sql_query/INSERT_INTO_subject.sql
+++ src/edit_db/sql_query/INSERT_INTO_subject.sql
@@ -1,3 +1,7 @@
 INSERT INTO "subject" (name) VALUES
 ('Объектно-ориентированный анализ и проектирование (ООАиП)'),
-('Natural language processing (NLP)');
+('Natural language processing (NLP)'),
+('Практикум по программированию'),
+('Алгоритмы и анализ сложности'),
+('WEB'),
+('Computer Vision');
